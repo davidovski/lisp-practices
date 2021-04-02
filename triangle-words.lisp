e96a028e595fdce8570d1c175c1555fd71f47324
davidovski <david@sendula.com>
2021-04-02 15:54:17 +0100

    done problem42

--- triangle-words.lisp
+++ triangle-words.lisp
@@ -1,1 +1,21 @@
-()
+(write	(with-open-file (stream "words.txt")
+	 (loop for line = (read-line stream nil)
+	  while line sum (if ( 
+			  (lambda (i)
+			   (= 0 (- i (floor i)))
+			  ) (+ -0.5 (
+					  sqrt (
+						  + 0.25 (
+							  * 2 (loop for c across (string-downcase line)  
+								  sum (+ 1 (position c "abcdefghijklmnopqrstuvwxyz"))
+							      )
+							 )
+					       )
+				    )
+			    )
+			  )
+		  1 0
+		  )
+	 )	
+	)
+)
